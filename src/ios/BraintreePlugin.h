28a01a3f1c4454595d1cd4ae94594bf872df63db
adam.lay <adam.lay@uniware.co.uk>
2018-05-17 16:07:51 +0100

    Adding iOS PayPal, version bump

--- src/ios/BraintreePlugin.h
+++ src/ios/BraintreePlugin.h
@@ -6,8 +6,14 @@
 
 #import <Foundation/Foundation.h>
 #import <Cordova/CDV.h>
+#import <BraintreeDataCollector/BraintreeDataCollector.h>
 
 @interface BraintreePlugin : CDVPlugin
+
+@property (nonatomic, strong) BTDataCollector *dataCollector;
+
 - (void)initialize:(CDVInvokedUrlCommand *)command;
 - (void)presentDropInPaymentUI:(CDVInvokedUrlCommand *)command;
+- (void)payPalProcess:(CDVInvokedUrlCommand *)command;
+- (void)payPalProcessVaulted:(CDVInvokedUrlCommand *)command;
 @end
--- src/ios/BraintreePlugin.m
+++ src/ios/BraintreePlugin.m
@@ -221,6 +221,122 @@ NSString *countryCode;
     [self.viewController presentViewController:dropIn animated:YES completion:nil];
 }
 
+-(void)initDataCollector:(NSString *)environment {
+    if ([environment isEqual: @"sandbox"]) {
+        self.dataCollector = [[BTDataCollector alloc] initWithEnvironment:BTDataCollectorEnvironmentSandbox];
+    }
+    else if ([environment isEqual: @"production"]) {
+        self.dataCollector = [[BTDataCollector alloc] initWithEnvironment:BTDataCollectorEnvironmentProduction];
+    }
+}
+
+-(void)payPalProcess:(CDVInvokedUrlCommand *)command {
+
+    NSString *chargeAmount = [command.arguments objectAtIndex:0];
+    NSString *chargeCurrency = [command.arguments objectAtIndex:1];
+    NSString *chargeEnvironment = [command.arguments objectAtIndex:2];
+
+    [self initDataCollector:chargeEnvironment];
+
+    BTPayPalDriver *payPalDriver = [[BTPayPalDriver alloc] initWithAPIClient:self.braintreeClient];
+
+    payPalDriver.viewControllerPresentingDelegate = self;
+    payPalDriver.appSwitchDelegate = self;
+
+    [payPalDriver authorizeAccountWithCompletion:^(BTPayPalAccountNonce *tokenizedPayPalAccount, NSError *error) {
+        if (tokenizedPayPalAccount == nil) {
+            [self userDidCancelPayment];
+        }
+
+        CDVPluginResult *pluginResult;
+
+        if (tokenizedPayPalAccount != nil) {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:@{
+                @"nonce": tokenizedPayPalAccount.nonce,
+                @"payerId": tokenizedPayPalAccount.payerId,
+                @"forename": tokenizedPayPalAccount.firstName,
+                @"surname": tokenizedPayPalAccount.lastName,
+                @"deviceData": [BTDataCollector payPalClientMetadataId]
+            }];
+        }
+
+        if (error != nil) {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        }
+
+        if (error || tokenizedPayPalAccount) {
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        }
+    }];
+
+    BTPayPalRequest *request = [[BTPayPalRequest alloc] initWithAmount:chargeAmount];
+    request.currencyCode = chargeCurrency;
+
+    [payPalDriver requestOneTimePayment:request completion:^(BTPayPalAccountNonce * _Nullable tokenizedPayPalAccount, NSError * _Nullable error) {
+
+    }];
+}
+
+-(void)payPalProcessVaulted:(CDVInvokedUrlCommand *)command {
+
+    NSString *chargeEnvironment = [command.arguments objectAtIndex:0];
+
+    [self initDataCollector:chargeEnvironment];
+
+    BTPayPalDriver *payPalDriver = [[BTPayPalDriver alloc] initWithAPIClient:self.braintreeClient];
+    payPalDriver.viewControllerPresentingDelegate = self;
+    payPalDriver.appSwitchDelegate = self;
+
+
+    [payPalDriver authorizeAccountWithCompletion:^(BTPayPalAccountNonce *tokenizedPayPalAccount, NSError *error) {
+        if (tokenizedPayPalAccount == nil) {
+            [self userDidCancelPayment];
+        }
+
+        CDVPluginResult *pluginResult;
+
+        if (tokenizedPayPalAccount != nil) {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:@{
+                @"nonce": tokenizedPayPalAccount.nonce,
+                @"payerId": tokenizedPayPalAccount.payerId,
+                @"forename": tokenizedPayPalAccount.firstName,
+                @"surname": tokenizedPayPalAccount.lastName,
+                @"deviceData": [BTDataCollector payPalClientMetadataId]
+            }];
+        }
+
+        if (error != nil) {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        }
+
+        if (error || tokenizedPayPalAccount) {
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        }
+    }];
+
+    BTPayPalRequest *request = [[BTPayPalRequest alloc] init];
+
+    [payPalDriver requestBillingAgreement:request completion:^(BTPayPalAccountNonce * _Nullable tokenizedPayPalAccount, NSError * _Nullable error) {
+
+    }];
+}
+
+-(void)userDidCancelPayment {
+    [self.viewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(void)paymentDriver:(id)paymentDriver requestsPresentationOfViewController:(UIViewController *)viewController {
+     [self.viewController presentViewController:viewController animated:YES completion:nil];
+}
+
+-(void)paymentDriver:(id)driver requestsDismissalOfViewController:(UIViewController *)viewController {
+    [self.viewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(void)openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication {
+
+}
+
 #pragma mark - PKPaymentAuthorizationViewControllerDelegate
 - (void)paymentAuthorizationViewController:(PKPaymentAuthorizationViewController *)controller didAuthorizePayment:(PKPayment *)payment completion:(void (^)(PKPaymentAuthorizationStatus status))completion {
     applePaySuccess = YES;
